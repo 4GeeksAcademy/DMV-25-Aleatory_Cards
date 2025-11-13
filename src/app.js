1053b0b693f25d5196e54f8588a8484a9dabadd5
DMV-25 <martinezvasquezdany@gmail.com>
2025-11-13 03:51:34 +0000

    avance random card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,8 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+let numeros= ["A", "2", "3", "4","5","6","7","8","9","10","J","Q","K"] 
+let Aleatorio= numeros[ Math.floor(Math.random() *numeros.length)]
+document.querySelector(".cartanumero").innerHTML=Aleatorio
   console.log("Hello Rigo from the console!");
 };
